0a6301cf14780b1328ee80d99feccaa59cc57d4b
Sylvester Amaechi <chinonso1amaechi@gmail.com>
2023-04-03 11:53:02 +0100

    fix issue on Job Repository binding

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,5 @@ Route::group([],function(){
     Route::get('/stories',[HackernewsController::class,'indexStories']);
     Route::get('/stories/{id}',[HackernewsController::class,'fetchById']);
 });
-// fix the cron job
-// fix the issue on parent_id
-//fix the issue on user
+
 
